554e4a8c553b8997cfbbbf0a12d9577a6486504f
cxl <1799359457@qq.com>
2020-06-27 18:21:34 +0800

    handler

---
+++ client/src/main/java/com/easyArch/client/ui/container/IdContainer.java
@@ -0,0 +1,7 @@
+package com.easyArch.client.ui.container;
+
+public class IdContainer {
+    public static final String RegisterView = "RegisterView";
+    public static final String LoginView = "LoginView";
+    public static final String MainView="MainView";
+}
---
+++ client/src/main/java/com/easyArch/client/ui/container/ResourceContainer.java
@@ -0,0 +1,31 @@
+package com.easyArch.client.ui.container;
+
+import javafx.scene.image.Image;
+
+public class ResourceContainer {
+    private static Image close = getImage("login/img/close.png");
+    private static Image close_1 = getImage("login/img/close_1.png");
+    private static Image min = getImage("login/img/min.png");
+    private static Image min_1 = getImage("login/img/min_1.png");
+
+    public static Image getMin_1() {
+        return min_1;
+    }
+
+    public static Image getClose() {
+        return close;
+    }
+
+    public static Image getClose_1() {
+        return close_1;
+    }
+
+    public static Image getMin() {
+        return min;
+    }
+
+    private static Image getImage(String resourcePath) {
+        return new Image(Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath));
+
+    }
+}
